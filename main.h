39aba703cb3d28f009adcad3fe688de07be226c1
rimdekhissi <rimdekhissi@outlook.fr>
2023-07-25 08:58:53 -0700

    main.h

--- main.h
+++ main.h
@@ -7,18 +7,18 @@
 #define UNUSED(x) (void)(x)
 #define BUFF_SIZE 1024
 
-/* FLAGS */
+/** FLAGS **/
 #define F_MINUS 1
 #define F_PLUS 2
 #define F_ZERO 4
 #define F_HASH 8
 #define F_SPACE 16
 
-/* SIZES */
+/** SIZES **/
 #define S_LONG 2
 #define S_SHORT 1
 
-/**
+/***
  * struct fmt - Struct op
  *
  * @fmt: The format.
@@ -31,7 +31,7 @@ struct fmt
 };
 
 
-/**
+/***
  * typedef struct fmt fmt_t - Struct op
  *
  * @fmt: The format.
@@ -43,9 +43,9 @@ int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
 
-/****************** FUNCTIONS ******************/
+/**************** FUNCTIONS ****************/
 
-/* Funtions to print chars and strings */
+/** Funtions to print chars and strings **/
 int print_char(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_string(va_list types, char buffer[],
@@ -53,7 +53,7 @@ int print_string(va_list types, char buffer[],
 int print_percent(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
-/* Functions to print numbers */
+/** Functions to print numbers **/
 int print_int(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_binary(va_list types, char buffer[],
@@ -106,7 +106,7 @@ int write_unsgnd(int is_negative, int ind,
 char buffer[],
 	int flags, int width, int precision, int size);
 
-/****************** UTILS ******************/
+/**************** UTILS ****************/
 int is_printable(char);
 int append_hexa_code(char, char[], int);
 int is_digit(char);
